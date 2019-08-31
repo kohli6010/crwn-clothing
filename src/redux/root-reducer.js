1fab2ab86f474a8ea6932da03f1ab68298eaec3d
unknown <kohli6010@gmail.com>
2019-08-31 19:11:31 +0530

    Added root Reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducer } from 'redux';
 import userReducer from './user/user-reducer';
 
-// combining the all reducer to send a giant store after making it ⏲
+// combining the all reducer to send a giant store after making it 🔆
 export default combineReducer({
 	user: userReducer
 })
