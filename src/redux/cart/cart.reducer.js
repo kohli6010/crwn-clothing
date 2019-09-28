a7c6d4fdfca31ca0f95328d1ddeed66293e15c6a
unknown <kohli6010@gmail.com>
2019-09-28 21:31:17 +0530

    Added qunatity field in the cartItems

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,3 +1,4 @@
+import { addItemToCart } from './cart.utils';
 const INITIAL_STATE = {
 	hidden: false,
 	cartItems: []
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case 'ADD_ITEM': { 
 			return {
 				...state,
-				cartItems: [...state.cartItems, action.payload]
+				cartItems: addItemToCart(state.cartItems, action.payload)
 			}
 		}
 			
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+	const existingItem = cartItems.find(cartItem => { 
+		return cartItem.id === cartItemToAdd.id;
+	})
+
+	if (existingItem) { 
+		return cartItems.map(cartItem => {
+			if (cartItem.id === cartItemToAdd.id) { 
+				return {...cartItem, quantity: cartItem.quantity+1}
+			}else{
+				return cartItem
+			}
+		})
+	}
+	return [...cartItems, {...cartItemToAdd, quantity: 1}]
+}
