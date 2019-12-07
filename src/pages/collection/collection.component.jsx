168d0c390b86253b68871fb810cd45090ad2a918
Rahul <kohli6010@gmail.com>
2019-12-07 23:48:18 +0530

    Added Collection Page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => { 
+	console.log(match)
+	return (
+		<div>
+			<h1>Collection Page</h1>
+		</div>
+	)
+}
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import { Route } from 'react-router-dom';
 
-const Shop = () => (
-    <div>
-        <CollectionOverview />
-    </div>
-);
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const Shop = ({ match }) => {
+    console.log(match);
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionOverview} />
+
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        </div>
+    );
+};
 
 export default Shop;
